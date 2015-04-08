a48b162be9d4d154b58f26cbfad66b4dd768efa2
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-04-08 07:37:16 +0200

    Optional commands

--- src/Command.hs
+++ src/Command.hs
@@ -1,20 +1,41 @@
-{-# LANGUAGE ExistentialQuantification, FlexibleInstances, MultiParamTypeClasses, Rank2Types, TypeFamilies, RecordWildCards #-}
+{-# LANGUAGE ExistentialQuantification, FlexibleInstances, MultiParamTypeClasses, Rank2Types, RecordWildCards, TemplateHaskell #-}
 
 module Command where
 
 import           Control.Applicative
 
+import           Data.Aeson.TH
 import qualified Data.Text as T
 import           Data.Maybe
 
 import           Web.Users.Types
 import           Web.PathPieces
 
+import           Model
+
+data Request = Request
+  { rqCommand :: T.Text
+  , rqOptions :: [(T.Text, T.Text)]
+  } deriving (Eq, Show)
+
+deriveJSON (opts { fieldLabelModifier     = rmvPrefix "rq"
+                 , constructorTagModifier = rmvPrefix ""}) ''Request
+
+data Response =
+    Ok
+  | Fail
+    { rspMessage  :: T.Text
+    } deriving (Eq, Show)
+
+deriveJSON (opts { fieldLabelModifier     = rmvPrefix "rsp"
+                 , constructorTagModifier = rmvPrefix ""}) ''Response
+
+
 data Option a b = Option
   { optName     :: T.Text
   , optDesc     :: T.Text
   , optDefault  :: b
-  }
+  } deriving (Eq, Show)
 
 type Opt a = Option a ()
 type OptMay a = Option (Maybe a) (Maybe a)
@@ -35,17 +56,12 @@ instance Lookupable (Maybe a) (Maybe a) where
   llkup opts (Option {..}) = ((read . T.unpack) <$> lookup optName opts) <|> Just optDefault
 
 class Exec bck opt optLk where
-  exec :: bck -> [(T.Text, T.Text)] -> Command opt optLk -> IO ()
-
-data BE = BE
-
-instance UserStorageBackend BE where
-  type UserId BE = String
+  exec :: bck -> [(T.Text, T.Text)] -> Command opt optLk -> IO Response
 
 instance (Read a, Read b, Read c, Read d, Read e, UserStorageBackend bck,
          Lookupable a d1, Lookupable b d2, Lookupable c d3, Lookupable d d4, Lookupable e d5) =>
          Exec bck (Option a d1, Option b d2, Option c d3, Option d d4, Option e d5)
-                  (a -> b -> c -> d -> e -> IO ()) where
-  exec bck os (Command {..}) = fromMaybe (return ()) $ do
+                  (a -> b -> c -> d -> e -> IO Response) where
+  exec bck os (Command {..}) = fromMaybe (return $ Fail "*** FIXME ERROR") $ do
     let (a, b, c, d, e) = cmdOptions
     cmdFn bck <$> llkup os a <*> llkup os b <*> llkup os c <*> llkup os d <*> llkup os e
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, FlexibleInstances, MultiParamTypeClasses, RecordWildCards, OverloadedStrings, TemplateHaskell, TypeOperators #-}
+{-# LANGUAGE DataKinds, FlexibleInstances, MultiParamTypeClasses, TypeFamilies, RecordWildCards, OverloadedStrings, TemplateHaskell, TypeOperators #-}
 
 module Main where
 
@@ -15,23 +15,6 @@ import qualified Data.Text as T
 import           Command
 import           Model
 
-data Request = Request
-  { rqCommand :: T.Text
-  , rqOptions :: [(T.Text, T.Text)]
-  }
-
-deriveJSON (opts { fieldLabelModifier     = rmvPrefix "rq"
-                 , constructorTagModifier = rmvPrefix ""}) ''Request
-
-data Response =
-    Ok
-  | Fail
-    { rspMessage  :: T.Text
-    }
-
-deriveJSON (opts { fieldLabelModifier     = rmvPrefix "rsp"
-                 , constructorTagModifier = rmvPrefix ""}) ''Response
-
 data UserData = UserData
   { usrNumber :: Maybe T.Text
   , usrSshKey :: Maybe T.Text
@@ -42,43 +25,24 @@ deriveJSON (opts { fieldLabelModifier     = rmvPrefix "usr"
 
 type Api = "user" :> Request :> Post Response
 
+data BE = BE
+
+instance UserStorageBackend BE where
+  type UserId BE = String
+
 cmdCreateUser = Command "create-user"
   ( Option "name" "User name" ()            :: Opt T.Text
   , Option "email" "User mail" ()           :: Opt T.Text
   , Option "password" "User password" ()    :: Opt T.Text
   , Option "number" "User number" Nothing   :: OptMay T.Text
   , Option "ssh-key" "User ssh key" Nothing :: OptMay T.Text
-  )
-  cmdCreateUserFn
-    where
-
-      -- cmdCreateUserFn :: UserStorageBackend bck => bck -> T.Text -> T.Text -> T.Text -> Maybe T.Text -> Maybe T.Text -> IO (Either CreateUserError (UserId bck))
-      cmdCreateUserFn b u_name u_email password usrNumber usrSshKey = do
-        createUser b (User { u_active = True
-                           , u_more   = UserData { .. }
-                           , u_password = makePassword $ PasswordPlain password
-                           , ..
-                           })
-        return ()
-
-{-
-parse :: UserStorageBackend b => b -> Request -> IO Response
-parse b (Request {..}) = undefined
-  where
-    lkp opt = lookup opt rqOptions
-
-    cmd "--create-user" = do
-      u_name     <- lkp "name"
-      u_email    <- lkp "email"
-      u_password <- makePassword . PasswordPlain <$> lkp "password"
-      usrNumber  <- pure <$> lkp "number"
-      usrSshKey  <- pure <$> lkp "ssh-key"
-
-      return $ createUser b (User { u_active = True
-                                  , u_more   = UserData { .. }
-                                  , ..
-                                  })
--}
+  ) $ \b u_name u_email password usrNumber usrSshKey -> do
+      createUser b (User { u_active = True
+                         , u_more   = UserData { .. }
+                         , u_password = makePassword $ PasswordPlain password
+                         , ..
+                         })
+      return Ok
 
 main :: IO ()
 main = do
