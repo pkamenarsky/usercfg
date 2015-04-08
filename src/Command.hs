98aae6d2c6dbc4e30973f78f89b0fdcf6788b120
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-04-08 18:21:18 +0200

    Applicative WIP

--- src/Command.hs
+++ src/Command.hs
@@ -44,6 +44,15 @@ type Keys = [(T.Text, T.Text)]
 
 data Resolve a = Resolve { unRes :: (Keys -> a) } deriving Functor
 
+class Read a => Lookupable a b where
+  llkup :: Read a => [(T.Text, T.Text)] -> Option a b -> Maybe a
+
+instance Read a => Lookupable a () where
+  llkup opts (Option {..}) = (read . T.unpack) <$> lookup optName opts
+
+instance Read a => Lookupable (Maybe a) (Maybe a) where
+  llkup opts (Option {..}) = ((read . T.unpack) <$> lookup optName opts) <|> Just optDefault
+
 opt :: Lookupable a b => Option a b -> Resolve a
 opt o = Resolve $ \keys -> fromJust $ llkup keys o
 
@@ -58,44 +67,5 @@ instance Monad Resolve where
 type Opt a = Option a ()
 type OptMay a = Option (Maybe a) (Maybe a)
 
-data Command bck opt optLk = Command
-  { cmdName     :: T.Text
-  , cmdOptions  :: opt
-  , cmdFn       :: bck -> optLk
-  }
-
-f :: String -> String -> Int
-f s s' = length s + length s'
-
-o1 = opt $ Option "name" "Name" ()
-o2 = opt $ Option "pass" "Password" ()
-
-f' = f <$> o1 <*> o2
-
 apply :: (Monad m, SequenceT a (m b), Curry (b -> c) d)  => a -> d -> m c
 apply opts f = sequenceT opts >>= return . uncurryN f
-
--- mkCmd :: opts -> (optsLk -> bck -> IO Response) -> (Keys -> IO Response)
--- mkCmd opts f = apply f optsI
-
-instance Show (Command bck a b) where
-  show = show . cmdName
-
-class Read a => Lookupable a b where
-  llkup :: Read a => [(T.Text, T.Text)] -> Option a b -> Maybe a
-
-instance Read a => Lookupable a () where
-  llkup opts (Option {..}) = (read . T.unpack) <$> lookup optName opts
-
-instance Read a => Lookupable (Maybe a) (Maybe a) where
-  llkup opts (Option {..}) = ((read . T.unpack) <$> lookup optName opts) <|> Just optDefault
-
-class Exec bck opt optLk where
-  exec :: Command bck opt optLk -> bck -> [(T.Text, T.Text)] -> IO Response
-
-instance (Lookupable a d1, Lookupable b d2, Lookupable c d3, Lookupable d d4, Lookupable e d5) =>
-         Exec bck (Option a d1, Option b d2, Option c d3, Option d d4, Option e d5)
-                  (a -> b -> c -> d -> e -> IO Response) where
-  exec (Command {..}) bck os = fromMaybe (return $ Fail "*** FIXME ERROR") $ do
-    let (a, b, c, d, e) = cmdOptions
-    cmdFn bck <$> llkup os a <*> llkup os b <*> llkup os c <*> llkup os d <*> llkup os e
--- src/Main.hs
+++ src/Main.hs
@@ -29,13 +29,7 @@ data BE = BE
 
 instance UserStorageBackend BE where
   type UserId BE = String
-
-cmd :: Exec bck a b => Command bck a b -> (T.Text, bck -> [(T.Text, T.Text)] -> IO Response)
-cmd cmd = (cmdName cmd, exec cmd)
-
-cmd' :: opts -> (optsLk -> IO Response) -> ([(T.Text, T.Text)] -> IO Response)
-cmd' opts f = undefined
-
+  createUser bck (User {..}) = return $ Right $ T.unpack $ u_name
 data Proxy a
 
 crUser :: UserStorageBackend bck => Resolve (bck -> IO Response)
@@ -51,38 +45,13 @@ crUser = apply
                              })
         return Ok
 
-cmds :: UserStorageBackend bck => Proxy bck -> [(T.Text, bck -> [(T.Text, T.Text)] -> IO Response)]
-cmds _ =
-  [ cmd $ Command "create-user"
-    ( Option "name" "User name" ()            :: Opt T.Text
-    , Option "email" "User mail" ()           :: Opt T.Text
-    , Option "password" "User password" ()    :: Opt T.Text
-    , Option "number" "User number" Nothing   :: OptMay T.Text
-    , Option "ssh-key" "User ssh key" Nothing :: OptMay T.Text
-    ) $ \b u_name u_email password usrNumber usrSshKey -> do
-        createUser b (User { u_active = True
-                           , u_more   = UserData { .. }
-                           , u_password = makePassword $ PasswordPlain password
-                           , ..
-                           })
-        return Ok
-  , cmd $ Command "delete-user"
-    ( Option "name" "User name" ()            :: Opt T.Text
-    , Option "email" "User mail" ()           :: Opt T.Text
-    , Option "password" "User password" ()    :: Opt T.Text
-    , Option "number" "User number" Nothing   :: OptMay T.Text
-    , Option "ssh-key" "User ssh key" Nothing :: OptMay T.Text
-    ) $ \b u_name u_email password usrNumber usrSshKey -> do
-        createUser b (User { u_active = True
-                           , u_more   = UserData { .. }
-                           , u_password = makePassword $ PasswordPlain password
-                           , ..
-                           })
-        return Ok
-  ]
+cmds :: UserStorageBackend bck => Proxy bck -> [(String, Resolve (bck -> IO Response))]
+cmds _ = [ ("create-user", crUser)
+         , ("delete-user", crUser)
+         ]
 
-names :: UserStorageBackend bck => bck -> [(T.Text, T.Text)] -> IO [Response]
-names bck opts = mapM (\(name, cmd) -> cmd bck opts) (cmds (undefined :: Proxy bck))
+names :: UserStorageBackend bck => bck -> Keys -> IO [Response]
+names bck opts = mapM (\(name, cmd) -> unRes cmd opts bck) (cmds (undefined :: Proxy bck))
 
 main :: IO ()
 main = do
