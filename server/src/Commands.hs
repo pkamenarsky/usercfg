3e26925e79014b09f830a3f25ca90e932eed5343
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-04-22 16:59:17 +0200

    Password reset

--- server/src/Commands.hs
+++ server/src/Commands.hs
@@ -2,15 +2,23 @@
 
 module Commands where
 
-import qualified Crypto.Hash.SHA1       as H
+import           Control.Applicative
+import           Control.Monad.Trans.Maybe
+import           Control.Monad.IO.Class
 
-import qualified Data.ByteString.Base16 as B16
-import qualified Data.Map               as M
-import qualified Data.Text              as T
-import qualified Data.Text.Encoding     as TE
+import qualified Crypto.Hash.SHA1           as H
+
+import qualified Data.ByteString.Base16     as B16
+import           Data.List                  (intercalate)
+import           Data.List.Split            (splitOn)
+import qualified Data.Map                   as M
+import qualified Data.Text                  as T
+import qualified Data.Text.Encoding         as TE
 import           Data.Tuple.OneTuple
 
-import qualified Network.Sendgrid.Api   as SG
+import qualified Network.Sendgrid.Api       as SG
+
+import           System.Environment
 
 import           Web.Users.Types
 
@@ -19,18 +27,28 @@ import           Command
 
 cmdResetPassword :: UserStorageBackend bck => (T.Text, Command bck (IO Response))
 cmdResetPassword = cmd "reset-password" True
-    (OneTuple $ opt "name" "n" "User name" None) $ \u_name bck -> do
-      token <- requestPasswordReset bck u_name 1000000
-      case token of
-        Just token' -> do
-          SG.sendEmail ( SG.Authentication "USERNAME" "PASSWORD" )
-                       $ SG.EmailMessage { to      = "owain@owainlewis.com"
-                                         , from    = ""
-                                         , subject = "Hello World"
-                                         , text    = T.unpack $ unPasswordResetToken token'
-                                         }
-          return responseOk
-        Nothing -> return responseOk
+    (OneTuple $ opt "name" "n" "User name" None) $ \username bck -> do
+      runMaybeT $ do
+        userId <- MaybeT $ getUserIdByName bck username
+        user   <- MaybeT $ getUserById bck userId :: MaybeT IO (User UserData)
+        token  <- MaybeT $ Just <$> requestPasswordReset bck userId 1000000
+
+        sgUser <- MaybeT $ lookupEnv "SG_USER"
+        sgPass <- MaybeT $ lookupEnv "SG_PASS"
+        sgFrom <- MaybeT $ lookupEnv "SG_FROM"
+        sgSubj <- MaybeT $ lookupEnv "SG_SUBJ"
+        sgText <- MaybeT $ lookupEnv "SG_TEXT"
+
+        liftIO $ SG.sendEmail (SG.Authentication sgUser sgPass)
+          $ SG.EmailMessage
+            { to      = T.unpack $ u_email user
+            , from    = sgFrom
+            , subject = sgSubj
+            , text    = intercalate ( T.unpack $ unPasswordResetToken token )
+                                    $ splitOn "$TOKEN" sgText
+            }
+
+      return responseOk
 
 cmdCreateUser :: UserStorageBackend bck => (T.Text, Command bck (IO Response))
 cmdCreateUser = cmd "create-user" False
@@ -57,5 +75,6 @@ cmdCreateUser = cmd "create-user" False
 commands :: UserStorageBackend bck => [(T.Text, Command bck (IO Response))]
 commands =
   [ cmdCreateUser
+  , cmdResetPassword
   , cmdAuth "delete-user" True noArgs $ \_ uid bck -> deleteUser bck uid >> return responseOk
   ]
