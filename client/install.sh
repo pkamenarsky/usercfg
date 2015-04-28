1c908e815563b15dd6f2b7ff75ad0e97887fc030
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-04-28 17:44:01 +0200

    Verbose output

--- client/install.sh
+++ client/install.sh
@@ -1,3 +1,4 @@
+set -o verbose
 pip install --user paramiko
 sudo curl https://raw.githubusercontent.com/pkamenarsky/usercfg/master/client/usercfg > /usr/local/bin/usercfg
 sudo chmod +x /usr/local/bin/usercfg
